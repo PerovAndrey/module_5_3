47520fb37614569e73dc12a70eee6d9514da74f3
PerovAndrey <nikita.vasiliev1982@mail.ru>
2024-09-10 14:59:29 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -18,22 +18,28 @@ class House:
         return f'Название {self.name}, количество этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, (House, int)):
+            return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, (House, int)):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, (House, int)):
+            return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, (House, int)):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, (House, int)):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, (House, int)):
+            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
         if isinstance(value, int):
@@ -41,8 +47,9 @@ class House:
             return self
 
     def __radd__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
     def __iadd__(self, value):
         if isinstance(value, int):
